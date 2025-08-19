ea22f1d33a369ef7c436baec566bb86d3e31de60
chirikatori <thanhluffy5D@gmail.com>
2025-08-19 15:23:25 +0700

    Text Recognition

--- backend/src/routes/ocr_routes.py
+++ backend/src/routes/ocr_routes.py
@@ -4,14 +4,14 @@ from fastapi import (
     UploadFile,
     HTTPException,
     Query,
-    Request
 )
-from fastapi.responses import FileResponse, JSONResponse
+from fastapi.responses import JSONResponse
 import os
-from src.ocr import EasyOCR, Magi  # noqa
-from src.utils import save_upload_file
-import glob
 import time
+
+from src.models.ocr import EasyOCR, Magi
+from src.utils import save_upload_file
+
 
 ocr_router = APIRouter()
 
@@ -23,10 +23,17 @@ os.makedirs(UPLOAD_DIR, exist_ok=True)
 async def text_detection(
     file: UploadFile = File(...), model_type: str = Query("magi")
 ):
+    # Save the uploaded file
     file_path = save_upload_file(file, UPLOAD_DIR)
     result_filename = f"result_{file.filename}"
     result_path = os.path.join(UPLOAD_DIR, result_filename)
 
+    # Validate file type
+    if not file.filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+        raise HTTPException(status_code=400,
+                            detail="Invalid file type. Only images are allowed.")
+
+    # Initialize the OCR engine based on the model type
     if model_type == "easyocr":
         ocr_engine = EasyOCR()
     elif model_type == "magi":
@@ -36,6 +43,8 @@ async def text_detection(
 
     try:
         ocr_engine.text_detection(file_path, result_path)
+
+        # Wait for the result file to be created
         for _ in range(20):
             if os.path.exists(result_path):
                 break
@@ -52,25 +61,44 @@ async def text_detection(
         raise HTTPException(status_code=500, detail=str(e))
 
 
+@ocr_router.post("/text-recognition")
+async def text_recognition(
+    file: UploadFile = File(...), model_type: str = Query("magi")
+):
+    # Save the uploaded file
+    file_path = save_upload_file(file, UPLOAD_DIR)
+    file_name, _ = os.path.splitext(file.filename)
+    result_filename = f"result_{file_name}.txt"
+    result_path = os.path.join(UPLOAD_DIR, result_filename)
 
+    # Validate file type
+    if not file.filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+        raise HTTPException(status_code=400,
+                            detail="Invalid file type. Only images are allowed.")
 
+    # Initialize the OCR engine based on the model type
+    if model_type == "easyocr":
+        ocr_engine = EasyOCR()
+    elif model_type == "magi":
+        ocr_engine = Magi()
+    else:
+        raise HTTPException(status_code=400, detail="Invalid model type")
 
-@ocr_router.get("/api/image/{filename}")
-async def get_image(filename: str):
-    search_pattern = os.path.join(UPLOAD_DIR,
-                                  f"{os.path.splitext(filename)[0]}.*")
+    try:
+        ocr_engine.text_recognition(file_path, result_path)
 
-    matching_files = glob.glob(search_pattern)
+        # Wait for the result file to be created
+        for _ in range(20):
+            if os.path.exists(result_path):
+                break
+            time.sleep(0.1)
 
-    if matching_files:
-        return FileResponse(matching_files[0])
-    return {"error": "File not found"}
+        if not os.path.exists(result_path):
+            raise HTTPException(status_code=500,
+                                detail="Result text file not found.")
 
-
-@ocr_router.post("/api/image/{filename}/delete")
-async def delete_image_via_post(filename: str, request: Request):
-    file_path = os.path.join(UPLOAD_DIR, filename)
-    if os.path.exists(file_path):
-        os.remove(file_path)
-        return {"message": f"File {filename} deleted successfully"}
-    raise HTTPException(status_code=404, detail="File not found")
+        return JSONResponse(
+            content={"processed_text_url": f"/api/image/{result_filename}"}
+        )
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
